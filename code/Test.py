540d79fd79c1f0ef226f87458171f97cf5c41f8a
shruthi-git-actions <85103629+shruthi-git-actions@users.noreply.github.com>
2021-06-09 15:30:33 +0530

    Add files via upload

---
+++ code/Test.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+from io import StringIO
+import dvc.api
+import requests
+import aiohttp
+import dask_ml
+import pandas as pd
+import dask.dataframe as dd
+from dask_ml.preprocessing import DummyEncoder
+import numpy as np
+from dask.distributed import Client
+from sklearn.ensemble import RandomForestClassifier
+import joblib
+import pickle
+import dask
+from dask_ml.metrics import accuracy_score
+import os, uuid
+import json
+from dask import dataframe as dd 
+import io
+# In[3]:
+#input
+with dvc.api.open(
+        'data/test_data.csv',
+        repo='https://github.com/shruthi-git-actions/dvc_v1.git',
+        remote='remote_storage',
+        rev="experiment",
+        encoding='utf-8'
+        ) as fd2:
+    main_test=pd.read_csv(fd2)
+#data
+with dvc.api.open(
+        'data/column_list_new.csv',
+        repo='https://github.com/shruthi-git-actions/dvc_v1.git',
+        remote='remote_storage',
+        rev="experiment",
+        encoding='utf-8'
+        ) as fd3:
+    columns_req=pd.read_csv(fd3)
+
+
+
+
+
+main_dfff = dd.from_pandas(main_test, npartitions=7)
+
+
+# In[4]:
+
+
+df=main_dfff[columns_req["Variable_list"]]
+
+
+# In[5]:
+
+
+no_columns=len(columns_req.index)
+
+
+# In[6]:
+
+
+
+
+
+# In[7]:
+
+
+x=df.iloc[:,0:no_columns-1]
+
+
+# In[8]:
+
+
+x=x.categorize()
+
+
+# In[9]:
+
+
+de = DummyEncoder()
+X_test = de.fit_transform(x)
+print("hi")
+
+# In[10]:
+
+#model
+with dvc.api.open("HumanEvent_Model.pkl", mode="rb") as f:
+  stream = io.BytesIO(f.read())
+clf = pickle.load(stream)
+'''
+clf = pickle.loads(
+    dvc.api.read(
+        'HumanEvent_Model.pkl',
+        repo='https://github.com/shruthi-git-actions/dvc_v1.git',
+        remote="remote_storage",
+        mode='rb'
+        )
+    )
+'''
+'''
+Pkl_Filename = "HumanEvent_Model.pkl"
+with open(Pkl_Filename, 'rb') as file:
+    clf = pickle.load(file)
+'''
+# In[11]:
+
+
+pred_test=clf.predict(X_test)
+
+
+# In[12]:
+
+
+pred_test_df=dd.from_array(pred_test)
+
+
+# In[13]:
+
+
+pred_test_df=pred_test_df.to_frame()
+
+
+# In[14]:
+
+
+prediction=x.merge(pred_test_df)
+
+
+# In[16]:
+
+
+
+
+
+# In[17]:
+
+
+
+prediction.to_csv("out1/prediction_new.csv", single_file = True)
+print ("Its Done")
+
+
---
+++ code/Train.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+from io import StringIO
+import requests
+import aiohttp
+import dask_ml
+import pandas as pd
+import dask.dataframe as dd
+from dask_ml.preprocessing import DummyEncoder
+import numpy as np
+from dask.distributed import Client
+from sklearn.ensemble import RandomForestClassifier
+import joblib
+import pickle
+import dask
+from dask_ml.metrics import accuracy_score
+import os, uuid
+import json
+import dvc.api
+import csv
+
+
+
+# In[ ]:
+#input
+with dvc.api.open(
+        'data/train_data.csv',
+        repo='https://github.com/shruthi-git-actions/dvc_v1.git',
+        remote='remote_storage',
+        rev="experiment",
+        encoding='utf-8'
+        ) as fd:
+    main_df=pd.read_csv(fd)
+#data
+with dvc.api.open(
+        'data/column_list_new.csv',
+        repo='https://github.com/shruthi-git-actions/dvc_v1.git',
+        remote='remote_storage',
+        rev="experiment",
+        encoding='utf-8'
+        ) as fd1:
+    columns_req=pd.read_csv(fd1)
+
+#Training
+main_dff = dd.from_pandas(main_df, npartitions=7)
+df=main_dff[columns_req["Variable_list"]]
+#df_test=main_test[columns_req["Variable_list"]]
+no_columns=len(columns_req.index)
+
+
+# In[ ]:
+
+
+x=df.iloc[:,0:no_columns-1]
+y_train=df.iloc[:,-1]
+#test_x=df_test.iloc[:,0:no_columns-1]
+#test_train=df_test.iloc[:,-1]
+
+
+# In[ ]:
+
+
+x=x.categorize()
+
+
+# In[ ]:
+
+
+de = DummyEncoder()
+X_train = de.fit_transform(x)
+
+
+# In[ ]:
+
+
+client = Client(processes=False)             # create local cluster
+
+clf = RandomForestClassifier(n_estimators=100, n_jobs=-1)
+with joblib.parallel_backend("dask", scatter=[X_train, y_train]):
+ clf.fit(X_train, y_train)
+
+
+# In[ ]:
+
+
+
+Pkl_Filename = "HumanEvent_Model.pkl"  
+
+with open(Pkl_Filename, 'wb') as file:  
+    pickle.dump(clf, file)
+
+
+# In[ ]:
+
+
+pred_train=clf.predict(X_train)
+
+
+# In[ ]:
+
+
+pred_train_df=dd.from_array(pred_train)
+pred_train_df=pred_train_df.to_frame()
+training=x.merge(pred_train_df)
+
+
+# In[ ]:
+
+
+
+
+training.to_csv("out/training_new.csv", single_file = True)
+
+
+# In[ ]:
+train_score=accuracy_score(np.array(y_train),np.array(pred_train))
+
+print(train_score)
+print ("The End ")
+ 
+
+
+
+
